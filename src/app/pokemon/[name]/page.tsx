5f0a05e96eb0f92a77c3109febe416f8065dfff2
Pedro Arenas <antonioaren2@gmail.com>
2025-03-22 22:59:12 +0100

    fix(PokemonDetailPage): Enhance type handling and streamline API data fetching

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,35 +1,36 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-//Cause of some issue to get the parameter inside the url with Next (params soon depreciated and use "use" 
-// needed to divided the code into 2 parts - SSR and CSR) 
-// I chose to keep it simple without useEffect or useState cause this page is not dynamic 
+//Cause of some issue to get the parameter inside the url with Next (params soon depreciated and use "use"
+// needed to divided the code into 2 parts - SSR and CSR)
+// I chose to keep it simple without useEffect or useState cause this page is not dynamic
 // after the first render so no need to complexify it)
 
-export default async function PokemonDetailPage({
-    params
-}: {
-    params: { name: string }
-}): Promise<React.ReactElement> {
-    const { name } = params
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    const pokemonData = await res.json()
+type Props = {
+	params: Promise<{ name: string }>;
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
-    console.log(pokemonData)
+export default async function PokemonDetailPage(
+	props: Props
+): Promise<React.ReactElement> {
+	const params = await props.params;
+	const { name } = params;
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+	const pokemonData = await res.json();
 
-    return (
-        <div className="container-pokemon flex flex-col items-center justify-center min-h-screen text-center">
-            <div className=''>
+	console.log(pokemonData);
 
-            </div>
-            <Image
-                src="/pikachu.png"
-                alt={name}
-                width={150}
-                height={150}
-                className="object-contain"
-            />
-            <h1>{name}</h1>
-            
-        </div>
-    )
+	return (
+		<div className="container-pokemon flex flex-col items-center justify-center min-h-screen text-center">
+			<div className=""></div>
+			<Image
+				src="/pikachu.png"
+				alt={name}
+				width={150}
+				height={150}
+				className="object-contain"
+			/>
+			<h1>{name}</h1>
+		</div>
+	);
 }
