67a5ce7a0a6bfaa7fa83d706d6dce5313b4815aa
Pedro Arenas <antonioaren2@gmail.com>
2025-03-22 22:49:13 +0100

    Refactor(pokemon/name): PokemonDetailPage to improve type definitions and fix API fetch URL

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,16 +1,17 @@
 import Image from 'next/image'
-import { PokemonPageParams } from "@/types/pokemon"
 
 //Cause of some issue to get the parameter inside the url with Next (params soon depreciated and use "use" 
 // needed to divided the code into 2 parts - SSR and CSR) 
 // I chose to keep it simple without useEffect or useState cause this page is not dynamic 
 // after the first render so no need to complexify it)
 
-export default async function PokemonDetailPage({ params }: PokemonPageParams) {
-
-    const { name } = params // Destructuring to avoid repeating "params."
-
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
+export default async function PokemonDetailPage({
+    params
+}: {
+    params: { name: string }
+}): Promise<React.ReactElement> {
+    const { name } = params
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     const pokemonData = await res.json()
 
     console.log(pokemonData)
@@ -28,7 +29,7 @@ export default async function PokemonDetailPage({ params }: PokemonPageParams) {
                 className="object-contain"
             />
             <h1>{name}</h1>
-
+            
         </div>
     )
 }
