67f2370d5a13e7f47f6761bd9703311173e26120
Idir FOUQUET <fouquetidir@gmail.com>
2025-03-23 18:32:51 +0100

    Fixes TS

--- src/features/pokemon/pokemonListSlice.ts
+++ src/features/pokemon/pokemonListSlice.ts
@@ -1,18 +1,25 @@
 /* eslint-disable */
 
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { PokemonState, FetchPokemonListResponse, EnrichedPokemonSummary } from '../../types/pokemon'
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  PokemonState,
+  FetchPokemonListResponse,
+  EnrichedPokemonSummary,
+} from "../../types/pokemon";
 
 export const fetchPokemonList = createAsyncThunk<EnrichedPokemonSummary[]>(
-    "pokemonList/fetchPokemonList",
-    async () => {
-      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
-      const data: FetchPokemonListResponse = await res.json();
-  
-      // enrichir chaque Pokémon avec son sprite
-      const enrichedResults = await Promise.all(
-        data.results.map(async (pokemon) => {
+  "pokemonList/fetchPokemonList",
+  async (): Promise<EnrichedPokemonSummary[]> => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data: FetchPokemonListResponse = await res.json();
+
+    const enrichedResults: EnrichedPokemonSummary[] = await Promise.all(
+      data.results
+        .filter(
+          (pokemon): pokemon is { name: string; url: string } =>
+            typeof pokemon.url === "string"
+        )
+        .map(async (pokemon) => {
           try {
             const res = await fetch(pokemon.url);
             const fullData = await res.json();
@@ -29,41 +36,38 @@ export const fetchPokemonList = createAsyncThunk<EnrichedPokemonSummary[]>(
             };
           }
         })
-      );
-  
-      return enrichedResults;
-    }
-  );
+    );
+
+    return enrichedResults;
+  }
+);
 
 const initialState: PokemonState = {
-    pokemonList: [],
-    loading: false, 
-    error: null as string | null, 
-}
+  pokemonList: [],
+  loading: false,
+  error: null as string | null,
+};
 
 const pokemonListSlice = createSlice({
-  name: 'pokemonList',
+  name: "pokemonList",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder
-        .addCase(fetchPokemonList.pending, (state) => {
-            state.loading = true
-            state.error = null
-        })
-        .addCase(fetchPokemonList.fulfilled, (state, action) => {
-            state.loading = false
-            state.pokemonList = action.payload
-            
-        })
-        .addCase(fetchPokemonList.rejected, (state, action) => {
-            state.loading = false
-            state.error = action.error.message || "Error: Impossible to fetch the data"
-        })
-        
-  }
+      .addCase(fetchPokemonList.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchPokemonList.fulfilled, (state, action) => {
+        state.loading = false;
+        state.pokemonList = action.payload;
+      })
+      .addCase(fetchPokemonList.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.error.message || "Error: Impossible to fetch the data";
+      });
   },
-  
-)
+});
 
-export default pokemonListSlice.reducer
+export default pokemonListSlice.reducer;
